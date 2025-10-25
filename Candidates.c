2a6ae743733217cff2cd09d64a1e2625611a4903
Suraj Sadeepa <suraj119sadipa@gmail.com>
2025-10-25 13:03:09 +0530

    Refactor candidate registration logic and validation

--- Candidates.c
+++ Candidates.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void logic();
+void remove_newline(char *str) {
+    int len = strlen(str);
+    if (len > 0 && str[len-1] == '\n') {
+        str[len-1] = '\0';
+    }
+}
+
+void candidate();
 
 void main()
 {
 
-    logic();
+    candidate();
 
 }
 
-void logic(){
+void candidate()
+{
     FILE *pfile = fopen("Candidates.txt", "a");
     char name[100];
     char nic[15];
     char number[5];
+    int party_number; // Use an integer for the number for better control
+
+    if (pfile == NULL) {
+        printf("\n\x1b[1;31m----! Error opening Candidates.txt !----\x1b[0m\n");
+        return;
+    }
+
     printf("\x1b[0m");
     printf("+------------------------------------------------------------------------------+\n");
     printf("|                                                                              |\n");
@@ -36,46 +52,69 @@ void logic(){
     printf("+ ---------------------------------------------------------------------------- +\n");
     printf("\n                   \x1b[1;32mPlease enter your details below\x1b[0m                            \n");
     printf("\x1b[0m");
+
+    // Clear input buffer before the first input (This was already present)
+    // while (getchar() != '\n'); // Removed this as it was after mainpage input, which is now fixed in mainpage.
+
     printf("\n                   1. Name: ");
-    scanf("%s", name);
-    printf("\n                   2. Political Party Name: ");
-    scanf(" %s", number);
+    fgets(name, sizeof(name), stdin);
+    remove_newline(name); // Remove newline
+
+    printf("\n                   2. Political Party Number: ");
+    // Use fgets to read the number as a string, then convert/validate
+    fgets(number, sizeof(number), stdin);
+    remove_newline(number);
+    party_number = atoi(number); // Convert to integer for easier validation
+
     printf("\n                   3. NIC: ");
-    scanf("%s", nic);
-    if(strlen(nic) != 12 || number[0] < '1' || number[0] > '5')
+    // Use fgets to read the NIC
+    fgets(nic, sizeof(nic), stdin);
+    remove_newline(nic);
+
+    // Basic input validation
+    // Changed condition to use party_number and strlen(nic)
+    if (strlen(nic) != 12 || party_number < 1 || party_number > 5)
     {
-        printf("\n                  *Invalid Input");
+        printf("\n                  \x1b[1;31m*Invalid NIC length (must be 12) or Invalid Party Number (must be 1-5).\x1b[0m\n");
     }
     else
-    {   
+    {
         char party[100];
 
-        switch (number[0])
+        // Determine party name based on number
+        switch (party_number) // Use the converted integer
         {
-            case '1':
-                strcpy(party, "National People's Power");
-                break;
-            case '2':
-                strcpy(party, "Samagi Jana Balawegaya");
-                break;
-            case '3':
-                strcpy(party, "New Democratic Front");
-                break;
-            case '4':
-                strcpy(party, "Sarvajana Balaya");
-                break;
-            default:
-                strcpy(party, "Democratic Tamil National Alliance");
-                break;
+        case 1:
+            strcpy(party, "National People's Power");
+            break;
+        case 2:
+            strcpy(party, "Samagi Jana Balawegaya");
+            break;
+        case 3:
+            strcpy(party, "New Democratic Front");
+            break;
+        case 4:
+            strcpy(party, "Sarvajana Balaya");
+            break;
+        case 5:
+            strcpy(party, "Democratic Tamil National Alliance");
+            break;
+
+        default:
+            // Should not happen due to validation above, but good practice
+            strcpy(party, "Unknown Party");
+            printf("\n\x1b[1;31minvalid number - data not saved\x1b[0m\n");
+            fclose(pfile);
+            return;
         }
 
-        //char text[] = "%s   %s    %s    %s", name, nic, number, party;
+        printf("\n");
+        // Write candidate details to file, using the original string representation for party number
         fprintf(pfile, "%-20s   %-12s    %-2s    %s\n", name, nic, number, party);
         printf("+ ---------------------------------------------------------------------------- +\n");
         printf("|                  \x1b[1;33mYour data saved successfully\x1b[0m                                |\n");
         printf("+------------------------------------------------------------------------------+\n");
     }
 
-    
     fclose(pfile);
 }
